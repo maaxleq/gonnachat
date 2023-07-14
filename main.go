842fadfc3168f1064642294f99ed7a2fad01fd81
Maxence Lequeux <maxenceleq@gmail.com>
2023-07-14 14:24:03 +0200

    Allow user to set the port

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-chi/chi"
 	"gonnachat/internal/handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
+const defaultPort = "3000"
+
 func main() {
+	port := os.Getenv("HTTP_PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
 	r := chi.NewRouter()
 
 	r.Get("/ws", handlers.WSChat)
 
-	err := http.ListenAndServe(":3000", r)
+	log.Printf("Server starting on port %s", port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), r)
 	if err != nil {
 		log.Fatal(err)
 	}
